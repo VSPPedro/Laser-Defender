12e6093623cad91e2401e4cc7e2993211802f6ec
Pedro Paiva <pedrovsp109@gmail.com>
2017-03-29 21:53:41 -0300

    Creating enemy positions

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour {
+
+	public float radius = 1f;
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireSphere (transform.position, radius);
+	}
+}
