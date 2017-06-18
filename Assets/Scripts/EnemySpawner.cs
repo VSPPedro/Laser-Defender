10b2f90801c3cc2b2c943637256942f268f429b8
Pedro Paiva <pedrovsp109@gmail.com>
2017-06-18 12:10:23 -0300

    Moving The Enemy Formation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 2f;
+
+	private bool movingRight = false;
+	private float xMax;
+	private float xMin;
 
 	// Use this for initialization
 	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint (new Vector3(0f, 0f, distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint (new Vector3(1f, 0f, distanceToCamera));
+		xMax = rightBoundary.x;
+		xMin = leftBoundary.x;
+
 		foreach (Transform child in transform) {
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity);
 			enemy.transform.parent = child;
 		}
 	}
-	
+
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0f));
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (movingRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+		float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+		float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+
+		if (rightEdgeOfFormation > xMax || leftEdgeOfFormation < xMin) {
+			movingRight = !movingRight;
+		}
 	}
 }
