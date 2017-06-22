3107ca08f7ebfaa5197fd84e91a14dc1b9c262dd
Pedro Paiva <pedrovsp109@gmail.com>
2017-06-22 17:12:22 -0300

    Shooting Enemies

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 200f;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile projectile = collider.GetComponent<Projectile> ();
+
+		if (projectile) {
+			health -= projectile.GetDamage ();
+			if (health <= 0) {
+				Destroy (gameObject);			
+			}
+			projectile.Hit ();
+		}
+	}
+}
